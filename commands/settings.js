4c74c84e8b90f64f68e9fe0b183f536f917511a2
devmdeniz <mehmetdeniz1397@gmail.com>
2021-01-27 02:01:58 +0300

    Log Channel Setter command translated to English

--- commands/settings.js
+++ commands/settings.js
@@ -5,35 +5,35 @@ exports.run = async (client, message, args) => {
  
  if (!message.member.hasPermission("MANAGE_MESSAGES")) {
   const bilgi = new Discord.RichEmbed()
-  .setDescription('Bu komutu kullanabilmek için **Mesajları Yönet** yetkisine sahip olmanız gerek.')
+  .setDescription('If you want use this command; You must have **Manage Messages** Permission.')
   .setColor("0000A0")
 return message.channel.sendEmbed(bilgi).then(m => m.delete(150000)); return
        }
   let mlog = message.mentions.channels.first()
   let sıfırla = db.fetch(`mlog_${message.guild.id}`)
-if(args[0] === "sıfırla") {
+if(args[0] === "reset") {
     if(!sıfırla) {
-      message.channel.send(`Log Kanalı zaten ayarlı değil.`)
+      message.channel.send(`Log Channel not already set.`)
                     
       return
     }
     db.delete(`mlog_${message.guild.id}`)
-    message.channel.send(`Log Kanalı başarıyla sıfırlandı.`)
+    message.channel.send(`Log channel resetted succesfully.`)
               
     return
   }
   if (!mlog) {
     return message.channel.send(
-    `Lütfen log kanalını etiketleyin`)                     
+    `Please tag the log channel`)                     
   }
   db.set(`mlog_${message.guild.id}`, mlog.id)
-  message.channel.send(`✅|Log Kanalı başarıyla ${mlog} olarak ayarlandı.`)
+  message.channel.send(`✅|Log Channel Succesfully setted ${mlog}`)
   };
   
 exports.conf = {
     enabled: true,
     guildOnly: true,
-    aliases: ['log'],
+    aliases: ['log', 'logset','logchannel'],
     permLevel: 0
 }
  
